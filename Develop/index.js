9ce17574087940b651db57dc300d99c99f3aa49f
Robert S <robscafe333@yahoo.com>
2024-05-30 08:24:27 -0700

    added the gitignore file, also in idex.js added two functions askPrerequisites and askPackagesNeededForApp - these two functions should work like confirm where a question is asked  and when no is entered  then the loop ends and answeres are pushed to corresponding arrays. Also created  an array of user questions. *Later on I will try and incorporate above functions  into the middle of the regular questions.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,84 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//In case there are more than one prerequisites this function loops through until confirm is "no".
+let prerequisites = [];
+
+// function that asks for prerequisite packages, example: node / npm
+
+function askPrerequisite() {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'prerequisite',
+                message:
+                    'What are the installation prerequisites? Example: npm',
+            },
+            {
+                type: 'confirm',
+                name: 'askAgain',
+                message:
+                    'Would you like to add another installation prerequisite?',
+                default: true,
+            },
+        ])
+        .then((answers) => {
+            askPrerequisite.push(answers.prerequisite);
+            if (answers.askAgain) {
+                return askPrerequisite();
+            } else {
+                return prerequisites;
+            }
+        });
+}
+
+function askPackagesNeededForApp() {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'prerequisite',
+                message:
+                    'What are the installation prerequisites? Example: Node.js? version?',
+            },
+            {
+                type: 'confirm',
+                name: 'askAgain',
+                message:
+                    'Would you like to add another installation prerequisite?',
+                default: true,
+            },
+        ])
+        .then((answers) => {
+            prerequisites.push(answers.prerequisite);
+            if (answers.askAgain) {
+                return askPrerequisite();
+            } else {
+                return prerequisites;
+            }
+        });
+}
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description?',
+    },
+    {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Enter table of contents?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
