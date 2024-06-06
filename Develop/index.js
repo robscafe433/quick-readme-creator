0c7fa93fad9d5d585a7a3f12acc3fb7ef9bde4ac
Robert S <robscafe333@yahoo.com>
2024-06-05 23:53:12 -0700

    In this commit I added an array for tableof contents as entries may vary as well and added code to treat this like a confirm with propmts to continue.  Also added two arrays for non repeating questions. The reason for this was to combine these two arrays into one object so asto be able to pass as data to next .then. Line 184

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,15 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { log } = require('console');
 
 //In case there are more than one prerequisites this function loops through until confirm is "no".
 let prerequisites = [];
 let packagesNeededForApp = [];
+let tableOfContents = [];
+
+let nonRepeatingAnswers1 = [];
+let nonRepeatingAnswers2 = [];
 
 // function that asks for prerequisite packages, example: node / npm
 
@@ -30,7 +35,33 @@ function askPrerequisite() {
             if (answers.askAgain) {
                 return askPrerequisite();
             } else {
+                console.log(prerequisites);
                 return prerequisites;
+            }
+        });
+}
+function askTableOfContents() {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'tableOfContents',
+                message: 'Enter content for the table: ',
+            },
+            {
+                type: 'confirm',
+                name: 'askAgain',
+                message: 'Would you like to add more content?',
+                default: true,
+            },
+        ])
+        .then((answers) => {
+            tableOfContents.push(answers.tableOfContents);
+            if (answers.askAgain) {
+                return askTableOfContents();
+            } else {
+                console.log(tableOfContents);
+                return tableOfContents;
             }
         });
 }
@@ -56,13 +87,14 @@ function askPackagesNeededForApp() {
             if (answers.askAgain) {
                 return askPackagesNeededForApp();
             } else {
+                console.log(packagesNeededForApp);
                 return packagesNeededForApp;
             }
         });
 }
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questionsFirstPart = [
     {
         type: 'input',
         name: 'projectTitle',
@@ -73,22 +105,138 @@ const questions = [
         name: 'description',
         message: 'What is the description?',
     },
+    // {
+    //     type: 'input',
+    //     name: 'tableOfContents',
+    //     message: 'Enter table of contents?',
+    // },
+];
+
+const questionsSecondPart = [
     {
         type: 'input',
-        name: 'tableOfContents',
-        message: 'Enter table of contents?',
+        name: 'usage',
+        message: 'What is the usage?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What are the licenses?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Do you wish to contribute? Enter your email:',
+    },
+    /// add readme.md section on tesitng, note: testing is not an input.
+    // The "Tests" section in a README.md file typically provides information
+    // on how to run the tests for the project.
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Have a question?',
     },
 ];
 
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(
+            questionsFirstPart
+            /* Pass your questions in here */
+            //Promise
+        )
+        .then((answers) => {
+            nonRepeatingAnswers1 = answers;
+            console.log(nonRepeatingAnswers1);
+
+            return askTableOfContents();
+
+            //originally did not have a return in front of function call
+            // and therefore was not waiting for the promise to be completed.
+            // Use user feedback for... whatever!!
+        })
+        .then(() => {
+            return askPrerequisite();
+        })
+        .then(() => {
+            return askPackagesNeededForApp();
+        })
+        .then(() => {
+            return inquirer.prompt(questionsSecondPart);
+        })
+        .then((answers) => {
+            nonRepeatingAnswers2 = answers;
+        })
+        .then(() => {
+            // I was attempting to combine all non-function-questions into one called
+            // nonRepeatingAnswersAll, here is code below. However, did not work.
+            // Reason: spread operator is used to unpack elements from "an array or object".
+            //Not what I wanted, In this case I wanted to combine "all" key/value pairs from
+            // both objects.
+            // nonRepeatingAnswersAll = (...nonRepeatingAnswers1, ...nonRepeatingAnswers2)
+
+            //To add two objects together into one the ".assign" must be used and if there are two values with same key,
+            // the second value will overwrite the first. *In the below case there are two objects, each with their own key/value pairs, are
+            // combined into one object with All key/value pairs.
+
+            return {
+                nonRepeatingAnswers: Object.assign(
+                    {},
+                    nonRepeatingAnswers1,
+                    nonRepeatingAnswers2
+                ),
+                prerequisites,
+                packagesNeededForApp,
+                tableOfContents,
+            };
+
+            // let prerequisites = [];
+            // let packagesNeededForApp = [];
+            // let tableOfContents = [];
+
+            // let nonRepeatingAnswers1 = [];
+            // let nonRepeatingAnswers2 = [];
+        })
+        .then((data) => {
+            //
+            console.log('////////////// Here is data : ', data);
+            const readMeTemplate = `
+            
+            # ${data.nonRepeatingAnswers.projectTitle}
+
+            ## User Story
+
+            \`\`\`md
+            AS A developer
+            I WANT a README generator
+            SO THAT I can quickly create a professional README for a new project.            
+            \`\`\`
+
+            
+
+            ## Description
+            ${data.nonRepeatingAnswers.description}
+
+
+            
+            `;
+            console.log(readMeTemplate);
+        });
+}
+
 // TODO: Create a function to write README file
-const writeToLog = (data) => {
+const writeToReadmeMD = (data) => {
     fs.writeFile('readme.md', data, (err) => {
-        err ? console.error(err) : console.log('Log created!');
+        err ? console.error(err) : console.log('Success!!! , Log created!');
     });
 };
 
-// TODO: Create a function to initialize app
-function init() {}
-
 // Function call to initialize app
 init();
+
+//still need below/
+
+/// add readme.md section on tesitng, note: testing is not an input.
+// The "Tests" section in a README.md file typically provides information
+// on how to run the tests for the project.
